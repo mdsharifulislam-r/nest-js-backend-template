fb9e3530c4675fbeed84b63d246879292481b226
md shariful Islam <eng.mdshariful.islam.7@gmail.com>
2026-04-20 17:43:45 +0600

    feat: final home page done

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,14 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
-import { ApiError } from './utlils/errors/api-error';
+import { Response } from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    throw new ApiError(HttpStatus.BAD_REQUEST, 'Hello World!');
+  getHello(res:Response) {
+   const htmlFilePath = join(process.cwd(),'src','utlils','template','home-template.html');
+   const html = readFileSync(htmlFilePath,'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.status(HttpStatus.OK).send(html);
   }
 }
